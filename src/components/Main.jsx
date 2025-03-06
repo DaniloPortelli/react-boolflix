847703a944b73e544fea178e6abb79554ac7d517
DaniloPortelli <daniloportelli@gmail.com>
2025-03-06 19:00:53 +0100

    inserimento div per hover

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,6 @@ const Main = () => {
     const { movies, tvs } = useGlobalContext()
     const urlBaseCopertina = import.meta.env.VITE_APP_BASE_URL_COPERTINA
 
-
     if (movies.length === 0 && tvs.length === 0) {
         return (
             <div>
@@ -15,25 +14,20 @@ const Main = () => {
         )
     } else {
 
-
         return (
-
-
             <main>
-                        <h1>Film</h1>
-                    <div className="cardContainer">
+                <h1>Film</h1>
+                <div className="cardContainer">
 
-                        {
-                            movies.map((elem) => {
+                    {
+                        movies.map((elem) => {
+                            let bandiera = (elem.original_language.toUpperCase())
+                            bandiera === "EN" ? bandiera = "GB" : bandiera
+                            bandiera === "JA" ? bandiera = "JP" : bandiera
 
-                                let bandiera = (elem.original_language.toUpperCase())
-                                bandiera === "EN" ? bandiera = "GB" : bandiera
-                                bandiera === "JA" ? bandiera = "JP" : bandiera
-
-
-                                return (
+                            return (
+                                <>
                                     <div key={elem.id} className="card">
-
                                         {/* <li>Titolo:{elem.title}</li>
                                             <li>Titolo originale:{elem.original_title}</li>
                                             <li>Lingua:
@@ -45,49 +39,58 @@ const Main = () => {
                                         <figure>
                                             <img className="imgCopertina" src={`${urlBaseCopertina}${elem.poster_path}`} alt={elem.original_title} />
                                         </figure>
-
                                     </div>
-                                )
-                            }
+
+                                    <div key={elem.id} className="cardData">
+                                        roba
+                                    </div>
+
+                                </>
                             )
                         }
-                    </div>
+                        )
+                    }
+                </div>
 
 
-                        <h1>Serie tv</h1>
-                    <div className="cardContainer">
-                        {
-                            tvs.map((elem) => {
-                                let bandiera = (elem.original_language.toUpperCase())
-                                bandiera === "EN" ? bandiera = "GB" : bandiera
-                                bandiera === "JA" ? bandiera = "JP" : bandiera
+                <h1>Serie tv</h1>
+                <div className="cardContainer">
+                    {
+                        tvs.map((elem) => {
+                            let bandiera = (elem.original_language.toUpperCase())
+                            bandiera === "EN" ? bandiera = "GB" : bandiera
+                            bandiera === "JA" ? bandiera = "JP" : bandiera
 
-                                return (
-                                    
-                                        // {/* <li>Titolo:{elem.name}</li>
-                                        //     <li>Titolo originale:{elem.original_name}</li>
-                                        //     <li>Lingua:
-                                        //         < img width={20}
-                                        //             alt={bandiera}
-                                        //             src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${bandiera}.svg`} />
+                            return (
 
-                                        //     </li>
-                                        //     <li>Voto:{(elem.vote_average / 2).toFixed()}/5</li> */}
-                                        <div key={elem.id} className="card">
-                                            <figure>
-                                                <img className="imgCopertina" src={`${urlBaseCopertina}${elem.poster_path}`} alt={elem.original_name} />
-                                            </figure>
-                                        </div>
+                                <>
 
-                                    
-                                )
-                            }
+                                // {/* <li>Titolo:{elem.name}</li>
+                                //     <li>Titolo originale:{elem.original_name}</li>
+                                //     <li>Lingua:
+                                //         < img width={20}
+                                //             alt={bandiera}
+                                //             src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${bandiera}.svg`} />
+                                //     </li>
+                                //     <li>Voto:{(elem.vote_average / 2).toFixed()}/5</li> */}
+                                    <div key={elem.id} className="card">
+                                        <figure>
+                                            <img className="imgCopertina" src={`${urlBaseCopertina}${elem.poster_path}`} alt={elem.original_name} />
+                                        </figure>
+                                    </div>
+
+
+                                    <div key={elem.id} className="cardData">
+                                        <h1>roba</h1>
+                                    </div>
+
+                                </>
+
                             )
                         }
-                    </div>
-
-
-
+                        )
+                    }
+                </div>
             </main>
         )
 
